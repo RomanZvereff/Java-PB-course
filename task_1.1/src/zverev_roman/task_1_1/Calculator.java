4edde7f4fd5f247abe2816506d8e7d13545e893f
RomanZvereff <romanzvereff@gmal.com>
2020-07-02 17:10:21 +0300

    task 1.1 done

---
+++ task_1.1/src/zverev_roman/task_1_1/Calculator.java
@@ -0,0 +1,60 @@
+package zverev_roman.task_1_1;
+
+import java.util.Scanner;
+
+public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double num1 = getNumber();
+        double num2 = getNumber();
+        char operation = getOperation();
+
+        switch(operation){
+            case '+':
+                System.out.println(num1 + num2);
+                break;
+            case '-':
+                System.out.println(num1 - num2);
+                break;
+            case '*':
+                System.out.println(num1 * num2);
+                break;
+            case '/':
+                if(num2 == 0){
+                    System.out.println("Деление на ноль недопустимо!");
+                    break;
+                }else{
+                    System.out.println(num1 / num2);
+                    break;
+                }
+            default:
+                System.out.println("Некорректная операция!");
+        }
+    }
+
+    public static double getNumber(){
+        double num = 0;
+        boolean isNotNumber;
+        do{
+            System.out.println("Введите число:");
+            num = scanner.nextDouble();
+            isNotNumber = false;
+        }while(isNotNumber);
+        return num;
+    }
+
+    public static char getOperation(){
+        char operation = '\u0000';
+        System.out.println("Введите арифметическую операцию (+, -, *, /):");
+        if(scanner.hasNextLine()){
+            operation = scanner.next().charAt(0);
+        }else{
+            getOperation();
+        }
+        return operation;
+    }
+
+
+
+}
